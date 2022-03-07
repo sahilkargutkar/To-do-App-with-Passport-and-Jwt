798cf95f241aec18c28e8d20f712d6c99f9e73b4
Sahil Kargutkar <sahilkargutkar.sk@gmail.com>
2022-03-07 21:27:10 +0530

    major changes

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,37 @@
+// import React from "react";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+import "./styles.css";
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+  return (
+    // <div className="todos">
+    //   {todos?.map((todo) => {
+    //     <SingleTodo
+    //       todo={todo}
+    //       key={todo.id}
+    //       todos={todos}
+    //       setTodos={setTodos}
+    //     />;
+    //   })}
+    // </div>
+
+    <div className="todos">
+      {todos?.map((todo) => (
+        <SingleTodo
+          todos={todos}
+          todo={todo}
+          key={todo.id}
+          setTodos={setTodos}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
